358925abecef076a6ca2729558775a11837d3230
Greg Wilson <greg.wilson@slalom.com>
2019-07-17 14:36:49 -0700

    created flask app; switched from venv to conda for env mgmt

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+import pandas as pd
+import boto3
+import pickle
+import os
+
+app = Flask(__name__)
+
+def get_model():
+    session = boto3.Session(aws_access_key_id=os.getenv('AWS_ADMIN_ACCESS'),
+                            aws_secret_access_key=os.getenv('AWS_ADMIN_SECRET'))
+
+    client = session.client('s3')
+
+    # TODO: make these environment vars
+    bucket = 'gwilson253awsprojects'
+    key = 'neptune/wine_model.pkl'
+
+    obj = client.get_object(Bucket=bucket, Key=key)
+    return pickle.loads(obj['Body'].read())
+
+model = get_model()
+
+@app.route('/')
+def index():
+    return 'yo!'
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        data = request.json
+        df = pd.read_json(data)
+        df['_pred'] = model.predict(df.values)
+        return df.to_json()
